3bd22839b935023c1b661d052ad7526a0246b3e6
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-16 19:12:36 +0530

    added configuration to use svg as imports

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      // Find the existing rule that handles SVG files
+      const svgFileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.(".svg")
+      );
+  
+      // Add a new rule to handle SVG files when the `?url` query is present
+      config.module.rules.push(
+        {
+          ...svgFileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // Only apply when "?url" is in the import query
+        },
+        {
+          test: /\.svg$/i,
+          issuer: svgFileLoaderRule.issuer,
+          resourceQuery: { not: [...svgFileLoaderRule.resourceQuery.not,/url/] }, // Apply when "?url" is NOT in the import query
+          use: ["@svgr/webpack"], // Use the SVGR loader for React components from SVG
+        }
+      );
+  
+      // Exclude `.svg` files from the original file loader rule
+      svgFileLoaderRule.exclude = /\.svg$/i;
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
