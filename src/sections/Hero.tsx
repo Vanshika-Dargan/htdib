dcd6e7872fa42e03e20d89d0ba8af081a27dfaa7
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-17 01:45:03 +0530

    added stars and planet

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/Button"
+import starryBg from "@/assets/stars.png"
 export const Hero = () => {
-    return <div></div>
+    return (
+       <section className="h-[492px] flex items-center" style={{
+        backgroundImage: `url(${starryBg.src})`
+       }}>
+        <div className="absolute h-64 w-64 bg-blue-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_79.8%_16.3%,white,rgb(158,221,255)_20.7%,rgb(24,0,66))] shadow-[20px_-20px_50px_rgb(255,255,255,.5),20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(79,117,255)]"></div>
+        <div className="container relative">
+        <div className="flex justify-center">
+        <h1 className="text-7xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(79,117,255,0.5))] text-transparent bg-clip-text text-center w-fit">ABCXYZ</h1>
+        </div>
+        <div className="flex items-center justify-center">
+        <p className="text-lg text-white/70 mt-5 text-center max-w-xl mx-auto">Generate Reports, Presentations, and Quizzes from PDFs, PowerPoints, YouTube, and Web Pages with AI-Powered Precision</p>
+        </div>
+        <div className="flex justify-center mt-5">
+        <Button>Get Started</Button>
+        </div>
+        </div>
+    </section>
+    )
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,6 +1,7 @@
 
 import Logo from '@/assets/logo.svg';
 import Menu from '@/assets/hamburger.svg'
+import { Button } from '@/components/Button';
 
 export const Navbar = () =>{
     return (
@@ -19,14 +20,7 @@ export const Navbar = () =>{
                     </nav>
                 </div>
                 <div className="flex gap-4 items-center">
-                <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#27005D] to-[#687EFF] shadow-[0px_0px_12px_#9EDDFF]">
-    <div className="absolute inset-0">
-        <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
-        <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
-        <div className="absolute inset-0 shadow-[inset_0_0_10px_#BEFFF7] rounded-lg"></div>
-    </div>
-    <span className="relative z-10">Get Started</span>
-</button>
+                <Button>Get Started</Button>
 
                     <Menu className="md:hidden"/>
                 </div>
